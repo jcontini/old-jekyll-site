6253f0400a54602751010c4abb34402599a82d1e
Joe Contini <efisio@gmail.com>
2015-02-17 18:19:48 -0500

    added back to top arrow

--- js/hc.js
+++ js/hc.js
@@ -1,14 +1,31 @@
 $(document).ready(function() {
     $(".container").hide();
     $(".container").fadeIn('5000');
-    $(".showcase-wrapper").hide();
-    $(".showcase-wrapper").fadeIn("slow");
 });
 
 /* Make external links open in a new window */
 $(document.links).filter(function() {
     return this.hostname != window.location.hostname;
 }).attr('target', '_blank');
+
+/* Scroll to Top feature */
+jQuery(document).ready(function() {
+    var offset = 250;
+    var duration = 300;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
 
 /*
 var toggle = false;
